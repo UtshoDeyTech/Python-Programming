f86a9d9df3ca993e276f0fc2392364877edd2e98
Utsho Dey <utshodey.tech@gmail.com>
2023-01-19 03:49:22 +0600

    new file added

---
+++ 04. Algorithms/Problem Solving/01_string_sort.py
@@ -0,0 +1,12 @@
+lst = [
+    'My name is Utsho dey',
+    'My name is Nur',
+    'He is a goo boy',
+    'He has a dog',
+    'Today is Friday',
+    'Today is friday',
+    'he has a dog'
+    ]
+
+# i = 'He is a goo boy'
+# print(lst.remove(i))
